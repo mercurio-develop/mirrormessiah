5b10cd7f11f4f3a5d3c06ccd9b8998841b09482f
Alejandro Fidanza <mercuriodevelop@gmail.com>
2026-06-07 11:41:53 -0500

    Fix layout hydration mismatch via suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`h-full dark`}>
+    <html lang="en" className={`h-full dark`} suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
